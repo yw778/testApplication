c1bec44557e57c5352faf285b89c15836642c74a
YUWANG <yuwang@dhcp-rhodes-394.eduroam.cornell.edu>
2016-08-06 17:16:49 -0400

    eeq

--- MNIST/cuda/sgd_single_point.cu
+++ MNIST/cuda/sgd_single_point.cu
@@ -251,15 +251,15 @@ static __global__ void p_SgdWithSharedParameterVector(
         // double step_size_times_prob_i_minus_label_i =
         //     (probability_of_positive - labels[point_idx]) * step_size;
         __syncthreads();
-        //debug use
-        //    if(relative_tidx==0&&blockIdx.x==0){
-        //     // printf("blockIdx.x is %d--tid is %d\n",blockIdx.x,threadIdx.x);
-        //     // printf("point_index is %d----%f\n",point_idx,labels[point_idx]);
-        //     for(size_t i=0; i<10;i++){
-        //         printf("after parameter is factored %f\n", probabilities_of_each[i]);
-        //     }   
-        // } 
-        // asm("trap;"); 
+        // debug use
+           if(relative_tidx==0&&blockIdx.x==0){
+            // printf("blockIdx.x is %d--tid is %d\n",blockIdx.x,threadIdx.x);
+            // printf("point_index is %d----%f\n",point_idx,labels[point_idx]);
+            for(size_t i=0; i<10;i++){
+                printf("after parameter is factored %f\n", probabilities_of_each[i]);
+            }   
+        } 
+        // asm("trap;")
 
         //debug use
         // printf("before update parameters \n");
