d9fe077290a1e15495c6ddd9dc49afd3b8e23d0e
YUWANG <yuwang@dhcp-rhodes-394.eduroam.cornell.edu>
2016-08-07 18:13:44 -0400

    s

--- MNIST/cuda/sgd_single_point.cu
+++ MNIST/cuda/sgd_single_point.cu
@@ -163,7 +163,7 @@ static __device__ void d_updateParameters(
     // asm("trap;"); 
 
      // debug use
-    if(relative_tidx==0&&blockIdx.x==0&&point_idx_in_block==1){
+    if(relative_tidx==0&&blockIdx.x==0&&point_idx_in_block==0){
         for(size_t i=0; i<num_features;i++){
             printf("p-%f--", parameter_vector[i]);
         }
