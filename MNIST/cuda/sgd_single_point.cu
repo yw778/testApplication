ef432462456dba1986f3013946159c5c742b140e
YUWANG <yuwang@YUWANGs-MacBook-Pro.local>
2016-08-07 01:08:04 -0400

    eeqq

--- MNIST/cuda/sgd_single_point.cu
+++ MNIST/cuda/sgd_single_point.cu
@@ -127,12 +127,12 @@ static __device__ void d_updateParameters(
             FeatureType gradient_times_step_size = data_point_i[j] 
                 * step_size_times_prob_i_minus_label_i[point_idx_in_block * LABEL_CLASS+i];
                // // //debug use
-            // if(relative_tidx==0&&blockIdx.x==0&&point_idx_in_block==1&&i==7){
-            //     printf("gradient_times_step_size is  %f\n",gradient_times_step_size);
-            //     printf("data_point_i[j] is  %f\n",data_point_i[j]);
-            //     printf("minus is  %f\n",step_size_times_prob_i_minus_label_i[point_idx_in_block * LABEL_CLASS+i]);
-            //     printf("+before add is %d -- %f\n",(j+i * num_features),parameter_vector[j+i * num_features]);
-            // }
+            if(relative_tidx==0&&blockIdx.x==0&&point_idx_in_block==1&&i==7){
+                printf("gradient_times_step_size is  %f\n",gradient_times_step_size);
+                printf("data_point_i[j] is  %f\n",data_point_i[j]);
+                printf("minus is  %f\n",step_size_times_prob_i_minus_label_i[point_idx_in_block * LABEL_CLASS+i]);
+                printf("+before add is %d -- %f\n",(j+i * num_features),parameter_vector[j+i * num_features]);
+            }
             // asm("trap;"); 
             // if(relative_tidx==0&&blockIdx.x==0){
             //     printf("before add is %d %f\n",j+i * num_features, parameter_vector[j+i * num_features]);
@@ -143,14 +143,14 @@ static __device__ void d_updateParameters(
             // if(relative_tidx==0&&blockIdx.x==0){
             //     printf("gradient is  %f\n",parameter_vector[j+i* num_features]);
             // }
-            // if(relative_tidx==0&&blockIdx.x==0&&point_idx_in_block==1&&i==7){
-            //     printf("-after add %d -- %f\n",(j+i * num_features),parameter_vector[j+i * num_features]);
-            // } 
+            if(relative_tidx==0&&blockIdx.x==0&&point_idx_in_block==1&&i==7){
+                printf("-after add %d -- %f\n",(j+i * num_features),parameter_vector[j+i * num_features]);
+            } 
         }
         // if(i==8)
         // asm("trap;"); 
     }
-    // asm("trap;"); 
+    asm("trap;"); 
 
      // debug use
     // if(relative_tidx==0&&blockIdx.x==0&&point_idx_in_block==1){
@@ -263,13 +263,13 @@ static __global__ void p_SgdWithSharedParameterVector(
             }
         }
         //debug use
-        if(relative_tidx==0&&blockIdx.x==0&&point_idx_in_block==1){
-            for(size_t i=10; i<20;i++){
-                printf("after parameter is factored %f\n", probabilities_of_each[i]);
-            }   
-            printf("label is %f\n",labels[point_idx]);
-        } 
-        asm("trap;");  
+        // if(relative_tidx==0&&blockIdx.x==0&&point_idx_in_block==1){
+        //     for(size_t i=10; i<20;i++){
+        //         printf("after parameter is factored %f\n", probabilities_of_each[i]);
+        //     }   
+        //     printf("label is %f\n",labels[point_idx]);
+        // } 
+        // asm("trap;");  
 
         // double step_size_times_prob_i_minus_label_i =
         //     (probability_of_positive - labels[point_idx]) * step_size;
