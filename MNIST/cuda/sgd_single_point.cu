a6744ab68bc6cc0d2e2568b45d74c8746a013b08
YUWANG <yuwang@dhcp-rhodes-394.eduroam.cornell.edu>
2016-08-06 20:09:10 -0400

    eeq

--- MNIST/cuda/sgd_single_point.cu
+++ MNIST/cuda/sgd_single_point.cu
@@ -230,7 +230,7 @@ static __global__ void p_SgdWithSharedParameterVector(
                     point_idx_in_block, LABEL_CLASS);
         //debug use
         if(relative_tidx==0&&blockIdx.x==0){
-            for(size_t i=10; i<20;i++){
+            for(size_t i=20; i<30;i++){
                 printf("shared memory is %f\n", probabilities_of_each[i]);
             }   
         } 
