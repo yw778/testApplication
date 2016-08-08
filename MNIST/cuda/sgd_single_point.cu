aa61d024377fd937a7b3fbb3b95e2f73f3585f22
YUWANG <yuwang@dhcp-rhodes-798.eduroam.cornell.edu>
2016-08-08 13:58:55 -0400

    s

--- MNIST/cuda/sgd_single_point.cu
+++ MNIST/cuda/sgd_single_point.cu
@@ -280,7 +280,7 @@ static __global__ void p_SgdWithSharedParameterVector(
             }
         }
         //debug use
-        if(relative_tidx==0){
+        if(relative_tidx==0&&blockIdx.x==0&&point_idx_in_block==1){
             for(size_t i=0; i<22;i++){
                 printf("after parameter is factored %f\n", probabilities_of_each[i]);
             }   
