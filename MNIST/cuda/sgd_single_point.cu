1fa2a1c1be299fdbd166db16841c773622b26b9d
YUWANG <yuwang@YUWANGs-MacBook-Pro.local>
2016-08-08 00:27:36 -0400

    s

--- MNIST/cuda/sgd_single_point.cu
+++ MNIST/cuda/sgd_single_point.cu
@@ -124,7 +124,7 @@ static __device__ void d_updateParameters(
      //    asm("trap;");
     // size_t m=0;
 
-    for(size_t i=LABEL_CLASS-1;i>=0;i--){
+    for(size_t i= (LABEL_CLASS-1);i>=0;i--){
         // i=9;
         for (size_t j = thread_offset; j < num_features; j+=threads_per_datapoint){
 
