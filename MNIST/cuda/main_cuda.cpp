76960e712d0a1297305b3672a4e15bfce07544a5
YUWANG <yuwang@YUWANGs-MacBook-Pro.local>
2016-08-11 00:30:36 -0400

    ss

--- MNIST/cuda/main_cuda.cpp
+++ MNIST/cuda/main_cuda.cpp
@@ -279,10 +279,10 @@ void runConvergenceTime(
     //     }
     // }
 
-    size_t batch_sizes[10] = {1, 2, 4, 8, 20, 30, 45, 50, 60, 100};
+    size_t batch_sizes[10] = {4, 5, 6, 8, 20, 30, 45, 50, 60, 100};
     for (size_t threads_per_datapoint = 32;
-        threads_per_datapoint <= 512;
-        threads_per_datapoint*=2) {
+        threads_per_datapoint <= 96;
+        threads_per_datapoint+=32) {
 
         training_options.config_params["threads_per_datapoint"]
         = threads_per_datapoint;
