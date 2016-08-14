00559ad54f83d8da40e7bbd7220477802823af21
YUWANG <yuwang@YUWANGs-MacBook-Pro.local>
2016-08-14 17:05:48 -0400

    ss

---
+++ MNIST/cuda/mbgd_2_2.h
@@ -0,0 +1,8 @@
+#ifndef MNIST_MBGD_2_2
+#define MNIST_MBGD_2_2
+
+#include "utils/mnist_defs.h"
+
+void trainParallelMiniBatchGradientDescent22(DataSet training_set, TrainingOptions training_options);
+
+#endif
