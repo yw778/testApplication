b03108c0efb92d776af0055a1f2298e832b573fc
YUWANG <yuwang@YUWANGs-MacBook-Pro.local>
2016-08-14 21:14:44 -0400

    ss

--- MNIST/cuda/mbgd_2_2.cu
+++ MNIST/cuda/mbgd_2_2.cu
@@ -186,15 +186,15 @@ static __device__ void d_gradientForMiniBatch2 (
                           tidx, j, num_thread_each_label);
 
 
-            // if(tidx < LABEL_CLASS){
-            //     if(labels[point_idx]==tidx){
-            //         probabilities_of_each[j * LABEL_CLASS+tidx]-=1;
-            //         // probabilities_of_each[point_idx_in_block * LABEL_CLASS+relative_tidx]*=step_size;
-            //     }else{                   
-            //         // probabilities_of_each[point_idx_in_block * LABEL_CLASS+relative_tidx]*=step_size;
-            //     }
-            // }
-            // __syncthreads();
+            if(tidx < LABEL_CLASS){
+                if(labels[point_idx]==tidx){
+                    probabilities_of_each[j * LABEL_CLASS+tidx]-=1;
+                    // probabilities_of_each[point_idx_in_block * LABEL_CLASS+relative_tidx]*=step_size;
+                }else{                   
+                    // probabilities_of_each[point_idx_in_block * LABEL_CLASS+relative_tidx]*=step_size;
+                }
+            }
+            __syncthreads();
         }    
 
         // probabilities_of_positive[i] = d_logisticFunction(*dot_product)
@@ -204,18 +204,18 @@ static __device__ void d_gradientForMiniBatch2 (
     // use the first LABEL_CLASS * batch_size thread to update
     // the probability matrix
     // the result is p=p-groundtruth
-    if(threadIdx.x < (LABEL_CLASS * batch_size)){
-        //calcuate the first  LABEL_CLASS * batch_size thread
-        // idx relative to the label
-        size_t tidx_class = threadIdx.x / LABEL_CLASS;
-        size_t relative_tidx_class = threadIdx.x % LABEL_CLASS;
+    // if(threadIdx.x < (LABEL_CLASS * batch_size)){
+    //     //calcuate the first  LABEL_CLASS * batch_size thread
+    //     // idx relative to the label
+    //     size_t tidx_class = threadIdx.x / LABEL_CLASS;
+    //     size_t relative_tidx_class = threadIdx.x % LABEL_CLASS;
 
-        if(labels[point_idx]==relative_tidx_class){
-                probabilities_of_each[tidx_class * LABEL_CLASS+relative_tidx_class]-=1;
-            }   
+    //     if(labels[point_idx]==relative_tidx_class){
+    //             probabilities_of_each[tidx_class * LABEL_CLASS+relative_tidx_class]-=1;
+    //         }   
 
-    }
-
+    // }
+    //debug use
     // if(tidx==0&&blockIdx.x==0){
     //         for (int i = 0; i < 20; ++i)
     //         {
