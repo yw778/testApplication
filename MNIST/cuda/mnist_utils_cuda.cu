55be63335206bfa56ab23f7789dbff5de8445800
YUWANG <yuwang@dhcp-rhodes-640.eduroam.cornell.edu>
2016-08-16 18:43:14 -0400

    ss

--- MNIST/cuda/mnist_utils_cuda.cu
+++ MNIST/cuda/mnist_utils_cuda.cu
@@ -40,9 +40,9 @@ void p_softmaxFunction2(cublasHandle_t handle,
     const size_t num_labels){
 
     for(size_t i=0;i< num_labels;i++){       
-        
+
         posibilities_positive[i] = p_dot_product(handle,
-            d_theta[i*num_feats],
+            &d_theta[i*num_feats],
             d_x_i,
             num_feats);
         // posibilities_positive[i] = a;
