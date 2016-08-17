1c0ae3fee6472aaa3378e812a2e1a2639b62de60
YUWANG <yuwang@YUWANGs-MacBook-Pro.local>
2016-08-16 22:49:12 -0400

    sss

--- MNIST/cuda/sgd_cublas.cu
+++ MNIST/cuda/sgd_cublas.cu
@@ -204,7 +204,7 @@ void trainStochasticGradientDescent3(
             // printf("i is %d\n",i);
             FeatureType* d_data_point_i = &d_data_points[i * training_set.num_features];
             // checkCudaErrors(cudaMemcpy(d_parameter_vector, training_set.parameter_vector,LABEL_CLASS * training_set.num_features * sizeof(FeatureType), cudaMemcpyHostToDevice));
-            p_gradientForSinglePoint(handle, d_parameter_vector, d_data_point_i, training_set.labels[i], training_set.num_features, d_gradient,d_result);
+            p_gradientForSinglePoint(handle, d_parameter_vector, d_data_point_i, d_result, training_set.labels[i], training_set.num_features, d_gradient);
             p_updateParameters(handle, d_parameter_vector, d_gradient, LABEL_CLASS * training_set.num_features, annealed_step_size);
         }
     }
