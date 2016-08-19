7a9bf03f5d525fe072e4c7142f254255dade0335
YUWANG <yuwang@dhcp-rhodes-844.eduroam.cornell.edu>
2016-08-19 17:53:34 -0400

    ss

--- MNIST/cuda/main_cuda.cpp
+++ MNIST/cuda/main_cuda.cpp
@@ -301,28 +301,28 @@ void runConvergenceTime(
     // }
     
 
-    for (size_t threads_per_datapoint = 163;
-        threads_per_datapoint <= 320;
-        threads_per_datapoint *= 2) {
+    // for (size_t threads_per_datapoint = 163;
+    //     threads_per_datapoint <= 320;
+    //     threads_per_datapoint *= 2) {
 
-        training_options.config_params["threads_per_datapoint"]
-        = threads_per_datapoint;
+    //     training_options.config_params["threads_per_datapoint"]
+    //     = threads_per_datapoint;
 
-        for (size_t datapoints_per_block = 1;
-            datapoints_per_block <= 4;
-            datapoints_per_block *= 2) {
+    //     for (size_t datapoints_per_block = 1;
+    //         datapoints_per_block <= 4;
+    //         datapoints_per_block *= 2) {
 
-            training_options.config_params["datapoints_per_block"]
-            = datapoints_per_block;
+    //         training_options.config_params["datapoints_per_block"]
+    //         = datapoints_per_block;
 
-            convergenceTime(
-                trainParallelStochasticGradientDescent2,
-                "CUDA SGD 2",
-                data_set,
-                training_options,
-                benchmark_options);
-        }
-    }
+    //         convergenceTime(
+    //             trainParallelStochasticGradientDescent2,
+    //             "CUDA SGD 2",
+    //             data_set,
+    //             training_options,
+    //             benchmark_options);
+    //     }
+    // }
 
     // size_t batch_sizes2[8] = {2, 4, 6, 8, 8, 9, 10, 11};
     // for (size_t threads_per_datapoint = 32;
