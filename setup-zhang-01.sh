df0747cc5545faae3a6637be2763ec1286ffe3a1
YUWANG <yuwang@dhcp-rhodes-1269.eduroam.cornell.edu>
2016-08-29 20:05:28 -0400

    ss

---
+++ setup-zhang-01.sh
@@ -0,0 +1,5 @@
+# setup-env for SDAccel Based OpenCL
+
+source /work/zhang/common/tools/xilinx/SDAccel/2016.1/settings64.sh
+export XILINX_SDACCEL=/work/zhang/common/tools/xilinx/SDAccel/2016.1/
+export XILINXD_LICENSE_FILE=$XILINXD_LICENSE_FILE:"/work/zhang/common/tools/xilinx/SDAccel/SDAccel-zhang01-eth0-20150818.lic"
