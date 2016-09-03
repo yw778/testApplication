445afe1fd656a1fc97a0a7628736a606ab6c4cf8
YUWANG <yuwang@YUWANGs-MacBook-Pro.local>
2016-09-03 16:13:49 -0400

    ss

--- testFilter/sdaccel-c/src/defs.h
+++ testFilter/sdaccel-c/src/defs.h
@@ -6,10 +6,10 @@
 
 // constants and default values for some configuration parameters
 #define NUM_FEATURES      1024
-#define NUM_SAMPLES       4500
-#define DATA_SET_SIZE     4608000
-#define NUM_TRAINING      4050
-#define NUM_TESTING       450
+#define NUM_SAMPLES       5000
+#define DATA_SET_SIZE     5120000
+#define NUM_TRAINING      4500
+#define NUM_TESTING       500
 #define NUM_VALIDATION    0
 #define LAMBDA            0 //regularization parameter
 #define STEP_SIZE         50 //step size (eta)
