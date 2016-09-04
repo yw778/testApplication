5bedabc879872c0d6b9bbfaafa01d6100802d4b0
YUWANG <yuwang@YUWANGs-MacBook-Pro.local>
2016-09-04 08:07:12 -0400

    ss

--- testFilter/sdaccel-c/src/main_sdaccel.cpp
+++ testFilter/sdaccel-c/src/main_sdaccel.cpp
@@ -98,6 +98,8 @@ int main(int argc, char *argv[]) {
         != NUM_SAMPLES)
             return EXIT_FAILURE;
 
+    printf("maximum size is %i\n",CL_DEVICE_MAX_MEM_ALLOC_SIZE);
+
     // memset(parameter_vector, 0, NUM_FEATURES * sizeof(FeatureType));
     for (size_t i = 0; i < NUM_FEATURES; i++)
         parameter_vector[i] = 0;
